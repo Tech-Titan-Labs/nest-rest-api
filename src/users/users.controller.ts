2bda8f1917798a11ca64793ec8fac42b0e2d501a
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-10-19 14:35:30 +0300

    message

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get() //get /users
+  findAll() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+//   @Post()
+//   create(@Body() user: object) {
+//     return user;
+//   }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate }
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
