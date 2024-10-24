a11176ac4c262c98f46a72613eaf062e246944c0
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-10-24 16:27:18 +0300

    controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SongModule } from './song/song.module';
 import { SongsController } from './songs/songs.controller';
 import { SongsModule } from './songs/songs.module';
 
 @Module({
   controllers: [AppController, SongsController],
   providers: [AppService],
-  imports: [SongModule, SongsModule],
+  imports: [SongsModule],
 })
 export class AppModule {}
--- src/songs/songs.controller.ts
+++ src/songs/songs.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Delete, Get, Put } from '@nestjs/common';
 
 @Controller('songs')
-export class SongsController {}
+export class SongsController {
+    @Get()
+    findAll(){
+        return 'find all songs endpoint';
+    }
+
+    @Get(":id")
+    findOne(){
+        return "fetch song based on Id";
+    }
+
+    @Put(':id')
+    update(){
+        return 'update based on id'
+    }
+
+    @Delete(':id')
+    delete(){
+        return 'delete based on id'
+    }
+}
