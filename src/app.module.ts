c195b705a00933d85e14a9d8da6ef4c439c3f9ac
G-KIBICHII <gcherogony@kabarak.ac.ke>
2024-10-26 02:48:54 +0300

    message

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
-import { Module } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 // import { SongsController } from './songs/songs.controller';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
   imports: [SongsModule],
 })
-export class AppModule {}
+// eslint-disable-next-line prettier/prettier
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    // throw new Error('Method not implemented.');
+    // consumer.apply(LoggerMiddleware).forRoutes('songs');
+    consumer.apply(LoggerMiddleware).forRoutes({path:'songs',method:RequestMethod.POST})
+  }
+}
